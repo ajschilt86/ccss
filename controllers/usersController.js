e7bca082b691662899ed9134fcc7c2985b8a2b22
Cloud Strife <mokryan17@gmail.com>
2018-10-30 00:24:41 -0500

    Almost finished w/ backend

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,7 +2,28 @@ const db = require("../models");
 
 module.exports = {
     findAll: (req, res) => {
-        db.answerSchema
+        db.Answers
             .find(req.query)
+            .sort({ date: -1 })
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    findById: (req, res) => {
+        db.Answers
+            .findById(req.params.id)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    create: (req, res) => {
+        db.Answers
+            .create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    update: (req, res) => {
+        db.Answers
+            .findOneAndUpdate({ _id: req.params.id }, req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
     }
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,2 +1,11 @@
 const router = requrie("express").Router();
-const 
+const userController = require("../../controllers/usersController");
+
+// Need to put a route here for displaying the quesitons...
+
+router
+    .route("/:id")
+    .get(userController.findById)
+    .put(userController.update)
+
+module.exports = router;
