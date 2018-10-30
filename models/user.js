f6fad12a59ff39ccba24ae5f765cf4d246caf484
ajschilt <ajschilt86@gmail.com>
2018-10-29 20:48:13 -0500

    added schema to user models

---
+++ models/user.js
@@ -0,0 +1,47 @@
+// Headline model
+// ==============
+
+// Require mongoose
+var mongoose = require("mongoose");
+
+// Create a schema class using mongoose's schema method
+var Schema = mongoose.Schema;
+
+// Create the headlineSchema with our schema class
+var answersSchema = new Schema({
+  // headline, a string, must be entered
+  question: {
+    type: String,
+    required: true
+  },
+  answer: {
+    type: String,
+    required: true
+  },
+  // summary, a string, must be entered
+  UID: {
+    type: String,
+    required: true
+  },
+  // url, a string, must be entered
+  industry: {
+    type: String,
+    required: true
+  },
+  // date is just a string  
+  email: {
+    type: String,
+    default: Date.now
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+  
+});
+
+// Create the Headline model using the headlineSchema
+var Answers = mongoose.model("Answers", answersSchema);
+
+// Export the Headline model
+module.exports = Answers;
