9be2105bb634856c1aaa01398128fec66da31aae
ajschilt <ajschilt86@gmail.com>
2018-11-05 09:17:11 -0600

    commented out a few lines of potentially useless imports

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Popover, PopoverInteractionKind, Position } from '@blueprintjs/core'
-import NewSongForm from './NewSongForm'
+// import { Popover, PopoverInteractionKind, Position } from '@blueprintjs/core'
+// import NewSongForm from './NewSongForm'
 
 class Header extends Component {
   constructor(props) {
