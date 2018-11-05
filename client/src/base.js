7388eb07b473592a508ceec69557dec921c3392f
ajschilt <ajschilt86@gmail.com>
2018-11-05 10:38:56 -0600

    added import to src/index.js

--- client/src/base.js
+++ client/src/base.js
@@ -1,19 +1,21 @@
 import Rebase from "re-base";
 import firebase from "firebase";
+import {} from "dotenv/config";
 
 const config = {
+  // apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  // authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  // databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
+
   apiKey: "AIzaSyDT2uchkwruMbZksYczEmpGvAQImtolb8w",
   authDomain: "ccss-a276b.firebaseapp.com",
   databaseURL: "https://ccss-a276b.firebaseio.com",
   projectId: "ccss-a276b",
   storageBucket: "ccss-a276b.appspot.com",
   messagingSenderId: "717832613113"
-  //   apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  //   authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-  //   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
-  //   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  //   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  //   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
 };
 
 const app = firebase.initializeApp(config);
