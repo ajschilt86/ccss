1451b749fa0f900b4dcb2e0d88972c6e2e7b5bfc
Nohabroha <noahysimon@gmail.com>
2018-11-10 12:16:39 -0600

    got answers displaying

--- client/src/pages/Questions/Questions.js
+++ client/src/pages/Questions/Questions.js
@@ -10,7 +10,6 @@ import Cards from "../../components/Card";
 
 
 class AnswerFeed extends Component {
-
     state = {
         answers: ["test1", "test2"],
         email: "",
@@ -18,38 +17,52 @@ class AnswerFeed extends Component {
     };
 
     componentDidMount() {
-        this.loadAnswers();
-    };
-
-    loadAnswers = () => {
-        API.getAnswersByQuest("pitch")
-            .then (res =>
-                this.state.answers.push(res.data),
-                console.log(this.state.answers)
-            )
-            .catch(err => 
-                console.log(err)
-            );
-            
-    };
-
-    render() {
-        return (
-            <div>
-                <h1>Share your 30 second pitch!</h1>
-                    <List>
-                        {this.state.answers.map(answers => (
-                            
-                            <h1>{answers}</h1>
-                            
-                        ))}
-                    </List>
-            
-            </div>
-        )
+        this.loadAnswers()
     }
 
+    loadAnswers() {
+        API.getAnswersByQuest("pitch")
+            .then(res => {
+                this.setState({
+                    answers: res.data
+                })
+            })
+    }
 
+    // // this.loadAnswers();
+    // loadAnswers = () => {
+    //     API.getAnswersByQuest("pitch")
+    //         .then(function (res) { 
+    //             // const dat = res.data[0]
+    //             // this.state.answers.push(dat)
+    //             // this.state.answers.push(res.data)
+    //             console.log(this.state)
+    //             console.log(res)
+    //             // console.log(res.data)
+    //             // console.log(typeof(res.data))
+    //         })
+    //         .catch(err => 
+    //             console.log(err)
+    //         );
+
+    // };
+
+
+
+
+render() {
+    return (
+        <div>
+            <h1>Share your 30 second pitch!</h1>
+            <List>
+                {this.state.answers.map((answer) => 
+                    <h1>{answer.answer}</h1>
+                )}
+            </List>
+        </div>
+    )
 }
+}
+
 
 export default AnswerFeed;
